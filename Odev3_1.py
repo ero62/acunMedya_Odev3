33797acde15a372c1d1806704ed05efaf452eacc
Ahmet Eren Arğun <ttdg6223@gmail.com>
2025-03-08 11:25:44 +0300

    Update Odev3_1.py

--- Odev3_1.py
+++ Odev3_1.py
@@ -5,10 +5,20 @@ with open("odev3.txt", "w") as f:
 with open("odev3.txt", "r") as f:
     print(f.read())
 
-with open("odev3.txt", "a") as f:
-    for i in range(6):
-        temp = input("lütfen metin giriniz: ")
-        f.write(temp + "\n")
+with open("odev3.txt", "a+") as f:
+    for i in range(5):
+        while True:
+            f.seek(0)
+            lines = {line.strip() for line in f}
+            temp = input("lütfen metin giriniz: ").strip()
+
+            if temp in lines :
+                print("farklı girini!")
+            else:
+                f.write(temp + "\n")
+                f.flush()
+                break
+
 
 with open("odev3.txt", "r") as f:
     for line in f:
